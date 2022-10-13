53f6d8052afe3b8e97ea994ca4eb87e38be05a20
Eva Malluta <eva.malluta14@gmail.com>
2022-10-13 16:19:05 +0200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import { PureComponent } from 'react';
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer, Tooltip } from 'recharts';
 import './App.css'
 const studentData = [
   {
@@ -34,8 +35,32 @@ const studentData = [
 // Using the Recharted library, create a graph as similar as you can, to the one in the #Classroom
 
 function App() {
-  return <>
-    {/* Code your solution here */}
+  return
+   <>
+   
+   <ResponsiveContainer width="100%" aspect={3}>
+        <BarChart
+          width={500}
+          height={300}
+          data={studentData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >            <>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="beavers" fill="#8884d8" />
+          <Bar dataKey="stars" fill="#82ca9d" />
+          </>
+
+        </BarChart>
+      </ResponsiveContainer>
   </>
 
 }
